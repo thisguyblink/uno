da896c7f6ca0c9a40f43917f4d7cd2bc867250ea
Patrick Smith <patricksmith@csu.fullerton.edu>
2024-04-21 00:36:36 -0700

    Made Home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    // You don't need to import the font here, you can import it directly in your CSS
+    // import 'https://fonts.googleapis.com/css2?family=Jersey+25&display=swap';
+</script>
+
+<div class="circle">
+    <p id="title">uno</p>
+</div>
+
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Jersey+25&display=swap");
+    * {
+    }
+    .circle {
+        padding: 150px;
+        position: relative;
+        margin: auto;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: black;
+        height: 300px;
+        width: 300px;
+        border-radius: 50%;
+    }
+    #title {
+        position: absolute;
+        top: 10%;
+        left: 33%;
+        color: white;
+        font-family: "Jersey 25";
+        font-weight: bold;
+        font-size: 150px;
+    }
+</style>
